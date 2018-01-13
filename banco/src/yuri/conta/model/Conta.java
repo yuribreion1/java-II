fd0ad6adba1531bea4c9fa21a2283f255ac82c56
yuribreion1 <yuribreion@gmail.com>
2018-01-13 20:54:35 -0200

    Ajuste no model

--- banco/src/yuri/conta/model/Conta.java
+++ banco/src/yuri/conta/model/Conta.java
@@ -2,12 +2,12 @@ package yuri.conta.model;
 
 import yuri.banco.interfaces.Autenticavel;
 
-abstract class Conta implements Autenticavel{
+abstract class Conta implements Autenticavel {
 	protected String nome;
 	protected double saldo;
 	protected double taxa;
-	
-	//Criando construtor para conta...
+
+	// Criando construtor para conta...
 	public Conta(String nome, double saldo, double taxa) {
 		this.nome = nome;
 		this.saldo = saldo;
@@ -19,10 +19,10 @@ abstract class Conta implements Autenticavel{
 	}
 
 	public void setTaxa(double taxa) {
-		this.taxa = taxa ;
+		this.taxa = taxa;
 	}
-	
-	public void atualiza(double taxa) { 
+
+	public void atualiza(double taxa) {
 		this.saldo += this.saldo * taxa;
 	}
 
@@ -31,12 +31,21 @@ abstract class Conta implements Autenticavel{
 	}
 
 	public void deposita(double valorDepositado) {
-		this.saldo += valorDepositado;
-		System.out.println("Deposito de " + valorDepositado + " feito com sucesso!");
+		if (valorDepositado > 0) {
+			this.saldo += valorDepositado;
+			System.out.println("Deposito de " + valorDepositado + " feito com sucesso!");
+		} else {
+			throw new RuntimeException("Valor incorreto para deposito");
+		}
 	}
-	
+
 	public void saca(double valorSacado) {
-		this.saldo -= valorSacado;
-		System.out.println("Saque de " + valorSacado + " feito com sucesso!\nUma taxa de " + taxa + " foi aplicada a transacao");
+		if (saldo >= valorSacado) {
+			this.saldo -= valorSacado;
+			System.out.println("Saque de " + valorSacado + " feito com sucesso!\nUma taxa de " + taxa
+					+ " foi aplicada a transacao");
+		} else {
+			throw new RuntimeException("Valor insuficiente");
+		}
 	}
 }
