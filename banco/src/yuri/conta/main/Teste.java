cf3fd1e122d4bc617d75b130006d974cf030d206
yuribreion1 <yuribreion@gmail.com>
2018-01-13 23:58:04 -0200

    Identacao de codigo

--- banco/src/yuri/conta/main/Teste.java
+++ banco/src/yuri/conta/main/Teste.java
@@ -10,29 +10,29 @@ public class Teste {
 	public static void main(String[] args) {
 
 		ContaCorrente cc = new ContaCorrente("Edmilson", 500, 0.15);
-		//ContaPoupanca cp = new ContaPoupanca("Mara", 5000, 0.16);
+		// ContaPoupanca cp = new ContaPoupanca("Mara", 5000, 0.16);
 		AtualizadorDeContas adc = new AtualizadorDeContas("Yuri", 500, 0.1);
 
 		try {
 			cc.saca(1200);
 		} catch (SaldoInsuficienteException e) {
 			System.err.println("O saldo esta insuficiente: " + e.getSaldoAtual());
-		} catch (Exception e) { 
+		} catch (Exception e) {
 			System.err.println("Outra Exception nao relacionada ao saldo");
 		}
-		
+
 		try {
 			cc.deposita(-500);
 		} catch (DepositoInvalidoException e) {
 			System.err.println("Valor depositado incorreto, favor refazer operacao: " + e.getValorDepositado());
-		} catch (Exception e) { 
+		} catch (Exception e) {
 			System.err.println("Outra exception nao relacionada ao deposito");
 		}
-		
+
 		cc.atualiza(0.01);
 
-		//adc.roda(cc);
-		//adc.roda(cp);
+		// adc.roda(cc);
+		// adc.roda(cp);
 
 		System.out.println("==========================================\n"
 				+ "Saldo total das contas com deducao de taxa selic: " + adc.getSaldoTotal());
